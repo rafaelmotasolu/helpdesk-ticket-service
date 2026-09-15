2a8248d0fb606755dd8f91e7a982e447437e0b4a
rafaelmotacr <rafaelmota361@gmail.com>
2026-09-15 17:04:14 -0300

    initial commit

---
+++ src/main/java/com/solutis/projeto/helpdesk_ticket_service/HelpdeskTicketServiceApplication.java
@@ -0,0 +1,13 @@
+package com.solutis.projeto.helpdesk_ticket_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelpdeskTicketServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelpdeskTicketServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/solutis/projeto/helpdesk_ticket_service/HelpdeskTicketServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.solutis.projeto.helpdesk_ticket_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelpdeskTicketServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
